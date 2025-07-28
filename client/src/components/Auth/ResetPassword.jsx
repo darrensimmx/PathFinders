83b935130a646b50d43f92a01e6f260004be7ce0
darren <darrensimmx@gmail.com>
2025-07-28 11:27:59 +0800

    removed /api for resetpw

--- client/src/components/Auth/ResetPassword.jsx
+++ client/src/components/Auth/ResetPassword.jsx
@@ -33,7 +33,7 @@ const ResetPassword = () => {
     }
 
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/reset-password`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/reset-password`, {
         token,
         newPassword
       });
